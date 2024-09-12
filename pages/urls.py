5dbc70670c6c892f9c9da544c552aa83efbc838f
AkylaiTopoeva <akylaytop@gmail.com>
2024-09-12 10:58:59 +0600

    views

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from pages.views import *
 
+
 urlpatterns = [
     path('helo/', help),
-    path("",homePageView, name='home')
+    path("",homePageView, name='home'),
+    path('1/', HomePageView.as_view(), name='homme'),
+    path('1/', AboutPageView.as_view(), name='about')
 ]
